bb0ed95303f1acd66cd0c14c409fd093643a7d7e
Demon-Qin <1792357514@qq.com>
2021-04-16 14:38:56 +0800

    版本回退

---
+++ pages/dynamic/dynamic.vue
@@ -0,0 +1,42 @@
+<template>
+	<uni-nav-bar>
+	<view slot="mod"> 
+	</view>
+	<view slot="right"> 
+	<i class="iconfont icon-bianji" @click="toPublish"></i>
+	</view>
+	</uni-nav-bar>
+</template>
+
+<script>
+	import uniNavBar from '../../components/uni-ui/uni-nav-bar/uni-nav-bar.vue'
+	export default {
+		components:{
+			uniNavBar
+		},
+		
+		data() {
+			return {
+				
+			}
+		},
+		// onNavigationBarButtonTap:function(){
+		//           uni.navigateTo({
+		//           	url:"./publish"
+		//           })   
+		//  },
+		methods: {
+			toPublish() {
+				uni.navigateTo({
+					url:"./publish"
+				})
+			}
+			
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
